0384fd2ed3d519809cd24aaf86df28459fa26a60
vishalinzalkar123 <vishalinzalkar123@gmail.com>
2020-09-03 15:11:38 +0530

    avdcd

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,8 @@
-	# config valid for current version and patch releases of Capistrano
+# config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
 set :application, "sunshine_medi_tech"
 set :repo_url, "git@github.com:vishalinzalkar123/sunshine_medi_tech.git"
-
 
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
@@ -12,6 +11,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
